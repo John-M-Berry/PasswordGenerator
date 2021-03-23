e4f457a140b2085e75b523f50829cab7016f92f8
MjrKong42 <mjr.kong42@gmail.com>
2021-03-23 09:45:25 -0500

    Added event listeners for Generate Password button as well as clipboard button. Initial version of project is complete.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function getRandomLower() {
 console.log(getRandomLower());
 
 function getRandomNumber() {
-  const numbers = `1234567890`;
+  const numbers = `0123456789`;
   randomIndex(numbers);
   return numbers[randomIndex(numbers)];
 }
@@ -93,7 +93,7 @@ function generatePassword(upper, lower, number, symbol, length) {
 
   // 3. Loop over the length and call the generator function for each checked type
   // Building password with a for loop
-  for (i = 0; i < 1; i += typesCount){
+  for (i = 0; i < 1; i += typesCount) {
     typesArr.forEach(type => {
       const funcName = type[0];
       console.log(funcName);
@@ -110,10 +110,6 @@ function generatePassword(upper, lower, number, symbol, length) {
   return finalPassword;
 }
 
-// generatePassword(true, false, true, false, 10);
-
-
-
 // Event listener for when the "Generate Password" button is clicked
 generateEl.addEventListener(`click`, () => {
   // Changing value from a string to a number
@@ -126,4 +122,35 @@ generateEl.addEventListener(`click`, () => {
   const hasSymbol = symbolsEl.checked;
 
   console.log(hasUpper, hasLower, hasNumber, hasSymbol, length);
+
+  // The generatePassword function takes the true/false values determined by the checkboxes as well as the number from the number input as arguments and returns a string (AKA The generated password) which is set as the value for the innerText property for the result element/span
+  resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
+
+});
+
+// Copy password to clipboard
+clipboardEl.addEventListener(`click`, () => {
+  const textArea = document.createElement(`textarea`);
+  const password = resultEl.innerText;
+
+  // If user clicks clipboard while no password is displayed the function will end and nothing will be copied to the clipboard
+  if (password === "") {
+    alert(`Please generate a password first`);
+    return;
+  }
+
+ // Setting the value for the textArea to the password that is currently being displayed
+ textArea.value = password;
+ // Selecting the body element
+ const body = document.querySelector(`body`);
+ // Adding the textarea to the webpage
+ body.append(textArea);
+ // Selecting the value inside the textarea
+ textArea.select();
+ // Copying the selected value
+ document.execCommand(`copy`);
+ // Removing the textarea element from the webpage/document
+ textArea.remove();
+ alert(`Password has been copied to the clipboard!`);
+
 });
